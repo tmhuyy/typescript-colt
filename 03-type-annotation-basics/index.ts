045dc87d2df32517d8450488b4fe6ccbac505cc0
Minh Huy Tran <huy.tranminh@gameloft.com>
2023-06-20 23:14:42 +0700

    update lecture 03

---
+++ 03-type-annotation-basics/index.ts
@@ -0,0 +1,18 @@
+let movieTitle: string = "Harry Potter";
+let firstNumber: number = 12;
+firstNumber++;
+const isFinish: boolean = true;
+// movieTitle = 123;
+
+let x = 27;
+// x = "27";
+
+const nameList = ["huy", "tu", "nguyen"];
+let myName: string;
+
+// for (let name of nameList) {
+//   if (name === "huy") {
+//     myName = name;
+//   }
+// }
+myName = "Huy";
