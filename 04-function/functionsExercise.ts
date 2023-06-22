705a5d10535ecb541f6328574410a2eeb1b1070b
Huy Tran <tmhuyy.itiu@gmail.com>
2023-06-22 08:41:46 +0700

    update function exercise

--- 04-function/functionsExercise.ts
+++ 04-function/functionsExercise.ts
@@ -4,7 +4,9 @@
 // Write a function called "twoFer" that accepts a person's name
 // It should return a string in the format "one for <name>, one for me"
 // If no name is provided, it should default to "you"
-
+const twoFer = (name: string = "you"): string => {
+  return `One for ${name}, one for me`;
+};
 // twoFer() => "One for you, one for me"
 // twoFer("Elton") => "One for Elton, one for me"
 
@@ -14,7 +16,14 @@
 // Write a isLeapyear() function that accepts a year and returns true/false depending on if the year is a leap year
 // isLeapYear(2012) => true
 // isLeapYear(2013) => false
-
+const isLeapYear = function (year: number): boolean {
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 4 === 0 && year % 100 !== 0) {
+    return true;
+  }
+  return false;
+};
 // To determine whether a year is a leapyear, use this "formula":
 // A YEAR IS A LEAPYEAR IF
 // - year is a multiple of 4 AND not a multiple of 100
