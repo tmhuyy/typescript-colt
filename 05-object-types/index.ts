f50346f4a263be6ba99be8cf65593e01ac92e91d
Huy Tran <tmhuyy.itiu@gmail.com>
2023-06-22 18:02:55 +0700

    + update intersection another types, optional property, readonly property. + upload object exercise

--- 05-object-types/index.ts
+++ 05-object-types/index.ts
@@ -26,3 +26,65 @@ const sendName = function (person: Person): Person {
 };
 
 sendName(userName);
+
+type PersonInfor = {
+  name: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  address?: {
+    street: string;
+    country: string;
+  };
+};
+
+const payloadInfor = (enteredInput: PersonInfor): void => {
+  console.log(
+    `My name is ${enteredInput.name.firstName}, I'm ${enteredInput.age} years old`
+  );
+};
+
+const huyInput: PersonInfor = {
+  name: {
+    firstName: "Huy",
+    lastName: "Tran",
+  },
+  age: 20,
+};
+
+payloadInfor(huyInput);
+type Game = {
+  name: string;
+  releasedYear: number;
+  //   readonly: mark the propertiy just can be read
+  readonly author: string;
+  stars?: number;
+};
+const pokemon: Game = {
+  name: "Pokemon",
+  releasedYear: 1995,
+  author: "Huy",
+};
+
+console.log(pokemon);
+// pokemon.author = "Tu";
+
+type Summer = {
+  isHot: boolean;
+};
+
+type Winter = {
+  isCold: boolean;
+};
+
+type Autumn = Summer &
+  Winter & {
+    isWindy: boolean;
+  };
+// combine them by &
+const autumn2023: Autumn = {
+  isCold: true,
+  isHot: false,
+  isWindy: false,
+};
